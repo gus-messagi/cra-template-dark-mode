2e0e1c21141cbbe05e99abbdb187e110efd0f396
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-04-14 19:44:04 -0300

    feat: added eslint based on airbnb

--- src/lib/redux/reducers/ThemeReducer.js
+++ src/lib/redux/reducers/ThemeReducer.js
@@ -2,8 +2,8 @@ import { CHANGE_THEME } from '../constants/theme';
 
 const initialState = localStorage.getItem('dark-mode') ? JSON.parse(localStorage.getItem('dark-mode')) : false;
 
-function ThemeReducer(state=initialState, action) {
-  switch(action.type) {
+function ThemeReducer(state = initialState, action) {
+  switch (action.type) {
     case CHANGE_THEME:
       localStorage.setItem('dark-mode', action.payload);
       return action.payload;
