0d7724634944c492966f1bf94f603cee616ef0b5
Gustavo Messagi <gustavo.ramos.messagi@gmail.com>
2020-04-17 22:18:24 -0300

    feat: added themeColors file to control the colors of the website

--- src/lib/components/pages/AppPage/AppPage.jsx
+++ src/lib/components/pages/AppPage/AppPage.jsx
@@ -1,24 +1,13 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
 import { connect } from 'react-redux';
+import { darkTheme, lightTheme } from '../../../utils/themeColors';
 import Page from '../../templates/Page';
 import Header from '../../templates/Header';
 
 // eslint-disable-next-line react/prop-types
 function AppPage({ theme }) {
   const html = document.querySelector('html');
-
-  const darkTheme = {
-    contentBackground: '#333',
-    headerBackground: '#111',
-    fontColor: '#fff',
-  };
-
-  const lightTheme = {
-    contentBackground: '#fff',
-    headerBackground: '#f1f1f1',
-    fontColor: '#333',
-  };
 
   const transformKey = (key) => `--${key.replace(/([A-Z])/, '-$1').toLowerCase()}`;
 
---
+++ src/lib/utils/themeColors.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  contentBackground: '#fff',
+  headerBackground: '#f1f1f1',
+  fontColor: '#333',
+};
+
+export const darkTheme = {
+  contentBackground: '#333',
+  headerBackground: '#111',
+  fontColor: '#fff',
+};
